f4dbe1e0bea603f95df596e61186973b57083184
shriraj2115 <163284295+shriraj2115@users.noreply.github.com>
2025-01-20 11:00:30 +0530

    es6

---
+++ Javascript/let.js
@@ -0,0 +1,10 @@
+// let a= 10;
+// console.log("a value is="+a);
+// let a=20;
+// console.log("a value is="+a);
+var a =10;
+for(let a=0;a<=5;a++){
+    console.log('a value is = '+a)
+}
+console.log('a value is = '+a)
+//Let keyword scope is only inside of the block, but not for the outside means global variable not accessible with let keyword. 
---
+++ Javascript/string.js
@@ -0,0 +1,12 @@
+// var str="Vasantdada Patil Engg College"
+// console.log(str);
+// console.log(str.length);
+// console.log(str.charAt(1));
+// console.log(str.startsWith("Vasantdada"));
+// console.log(str.endsWith("College"));
+// console.log(str.endsWith("college"));
+// console.log(str.includes("Patil"))
+// console.log(str.repeat(2))
+
+let collegeName="Vasantdada Patil Pratisthans College Of Engg."
+console.log(`I studied in ${collegeName} in Mumbai`)
